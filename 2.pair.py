c598f1dadb05482849d121e93e40a7e4413546b0
oaxiom <oaxiom@localhost>
2019-01-24 10:25:40 +0800

    added stats output

--- 2.pair.py
+++ 2.pair.py
@@ -35,6 +35,12 @@ def collect_valid_pairs(bam1_filename, bam2_filename, min_dist=5000):
     output = []
 
     # We assume the bam files are sorted by name and unaligned were also output
+    stats_aligned = 0
+    stats_1aligned = 0
+    stats_unaligned = 0
+    reject_diff_chrom = 0 
+    reject_too_close = 0
+
     done = 0
     while done < 10:
         read1 = bf1.__next__()
@@ -45,15 +51,34 @@ def collect_valid_pairs(bam1_filename, bam2_filename, min_dist=5000):
             print('ERORR: Mismatched read names (%s != %s), make sure the BAMs contain unaligned and are sorted by name' % (read1.query_name, read2.query_name))
             sys.quit()
 
+        # First, check both reads are aligned
+        if read1.is_unmapped and read2.is_unmapped:
+            stats_unaligned += 1
+            continue
+        elif read1.is_unmapped or read2.is_unmapped:
+            stats_1aligned += 1
+            continue
+        stas_aligned += 1
+        
         # criteria1: Must be on same chrom
         if read1.reference_name != read2.reference_name:
             continue
-        
+    
         done += 1
         
 
     bf1.close()
     bf2.close()
+
+    print('collect_valid_pairs() stats:')
+    print('  Aligned:')
+    print('    Reads processed : {:,}'.format(done))
+    print('    Correctly paired: {:,}'.format(stats_aligned))
+    print('    One pair aligned: {:,}'.format(stats_1aligned))
+    print('    No pairs aligned: {:,}'.format(stats_unaligned))
+    print('  Criteria rejected:')
+    print('    Different chrom : {:,}'.format(reject_diff_chrom))
+    print('    Too close       : {:,}'.format(reject_too_close))
 
     return output
     
@@ -61,6 +86,8 @@ def save_valid_pairs(pairs, output):
     pass
 
 if __name__ == '__main__':
+    work_path = sys.argv[0]
+
     if len(sys.argv) != 4:
         print('Not enough arguments!')
         print('2.pair.py bam1 bam2 output.bam')
