94d179275d825992ca48ba861db1a7fd51e8ece7
oaxiom <oaxiom@localhost>
2019-01-26 09:30:38 +0800

    parse once and report % of total reads

--- quantify_links.py
+++ quantify_links.py
@@ -49,12 +49,15 @@ class quantify:
         te = {'TE <-> TE': 0,
             'TE <-> -': 0,
             '-  <-> -': 0}
+        res_te_te = {}
+        res_te_nn = {}
 
-        print("Crude measures...")
+        print("Measures anchors...")
         total = 0
         oh = open(self.filename, 'r')
         for line in oh:
             r = line.strip().split('\t')
+            # measure TE anchors
             if 'TE' in r[4] and 'TE' in r[8]:
                 te['TE <-> TE'] += 1
             elif 'TE' in r[4] or 'TE' in r[8]:
@@ -65,32 +68,8 @@ class quantify:
             if total % 1000000 == 0:
                 print('Processed: {:,}'.format(total))
                 #break
-        oh.close()
 
-        print('\nmeasure_te_anchors():')
-        print('  TE <-> TE : {:,} ({:.2%})'.format(te['TE <-> TE'], te['TE <-> TE']/total))
-        print('  TE <-> -- : {:,} ({:.2%})'.format(te['TE <-> -'], te['TE <-> -']/total))
-        print('  -- <-> -- : {:,} ({:.2%})'.format(te['-  <-> -'], te['-  <-> -']/total))
-        print()
-
-        oh = open('%s_crude_measures.txt' % self.project_name, 'w')
-        oh.write('TE <-> TE : {:,} ({:.5%})\n'.format(te['TE <-> TE'], te['TE <-> TE']/total))
-        oh.write('TE <-> -- : {:,} ({:.5%})\n'.format(te['TE <-> -'], te['TE <-> -']/total))
-        oh.write('-- <-> -- : {:,} ({:.5%})\n'.format(te['-  <-> -'], te['-  <-> -']/total))
-        oh.close()
-
-    def measure_te_freqs(self):
-        '''
-        **Purpose**
-            Measure the types of TE frequencies, between pairs of TE -> TE and TE -> -
-        '''
-        print('Pair-wise measures')
-        res_te_te = {}
-        res_te_nn = {}
-        done = 0
-        oh = open(self.filename, 'r')
-        for line in oh:
-            r = line.strip().split('\t')
+            # MEasure TEs in detail
             if 'TE' in r[4] and 'TE' in r[8]:
                 # possible to have more than one TE:
                 tel = [i.strip() for i in r[3].split(',') if ':' in i] # can also hoover up some genes, so use ':' to discriminate TEs
@@ -116,24 +95,33 @@ class quantify:
                         res_te_nn[t] = 0
                     res_te_nn[t] += 1
 
-            done += 1
-            if done % 1000000 == 0:
-                print('Processed: {:,}'.format(done)) 
-                #break
+        oh.close()
+
+        print('\nmeasure_te_anchors():')
+        print('  TE <-> TE : {:,} ({:.2%})'.format(te['TE <-> TE'], te['TE <-> TE']/total))
+        print('  TE <-> -- : {:,} ({:.2%})'.format(te['TE <-> -'], te['TE <-> -']/total))
+        print('  -- <-> -- : {:,} ({:.2%})'.format(te['-  <-> -'], te['-  <-> -']/total))
+        print()
+
+        oh = open('%s_crude_measures.txt' % self.project_name, 'w')
+        oh.write('TE <-> TE : {:,} ({:.5%})\n'.format(te['TE <-> TE'], te['TE <-> TE']/total))
+        oh.write('TE <-> -- : {:,} ({:.5%})\n'.format(te['TE <-> -'], te['TE <-> -']/total))
+        oh.write('-- <-> -- : {:,} ({:.5%})\n'.format(te['-  <-> -'], te['-  <-> -']/total))
         oh.close()
 
         oh_te_te = open('%s_te-te_anchor_frequencies.tsv' % self.project_name, 'w')
-        oh_te_te.write('%s\n' % '\t'.join(['TE1', 'TE2', 'count']))
+        oh_te_te.write('%s\n' % '\t'.join(['TE1', 'TE2', 'count', '%']))
         for k in sorted(list(res_te_te)):
-            oh_te_te.write('%s\t%s\t%s\n' % (k[0], k[1], res_te_te[k]))
+            oh_te_te.write('%s\t%s\t%s\t%.6f\n' % (k[0], k[1], res_te_te[k], res_te_te[k]/total*100.0))
         oh_te_te.close()
 
         oh_te_nn = open('%s_te-nn_anchor_frequencies.tsv' % self.project_name, 'w')
-        oh_te_nn.write('%s\n' % '\t'.join(['TE1', 'count']))
+        oh_te_nn.write('%s\n' % '\t'.join(['TE1', 'count', '%']))
         for k in sorted(list(res_te_nn)):
-            oh_te_nn.write('%s\t%s\n' % (k, res_te_nn[k]))
+            oh_te_nn.write('%s\t%s\t%.6f\n' % (k, res_te_nn[k], res_te_nn[k]/total))
         oh_te_nn.close()
 
+        return
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -145,6 +133,5 @@ if __name__ == '__main__':
     q = quantify(sys.argv[2])
     q.load_tsv(sys.argv[1])
     q.measure_te_anchors()
-    q.measure_te_freqs()
 
 
