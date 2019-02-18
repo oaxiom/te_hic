b7f1418eb9637acb287fa34f27dc3bfc93e4ccd2
oaxiom <oaxiom@localhost>
2019-02-18 10:37:07 +0800

    dumb bug

--- common.py
+++ common.py
@@ -7,7 +7,6 @@ def print_species():
     print('    mm10 - mouse')
 
 def check_species(species):
-    species = sys.argv[1]
     if species not in ('mm10', 'hg38'):
         print('Species "%s" not found' % species)
         print_species()
