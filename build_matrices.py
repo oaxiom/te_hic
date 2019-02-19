64e1dcc24161a768ec4b5141597afe3dcbf46aa3
oaxiom <oaxiom@localhost>
2019-02-19 09:04:05 +0800

    working build_matrices tool

--- build_matrices.py
+++ build_matrices.py
@@ -67,8 +67,9 @@ class build_matrices:
         '''
         print('Building in-memory matrices')
 
+        done = 0
         oh = open(infilename, 'r')
-        for done, line in enumerate(oh):
+        for line in oh:
             line = line.strip().split('\t')
 
             # The format of the TE file is:
@@ -79,20 +80,36 @@ class build_matrices:
             read1_mid = (int(line[1]) + int(line[2])) // 2
             read2_mid = (int(line[6]) + int(line[7])) // 2
 
-            read1_bin = (read1_mid // self.res) + self.chrom_bin_offsets[read1_chrom]
-            read2_bin = (read2_mid // self.res) + self.chrom_bin_offsets[read2_chrom]
+            read1_bin = (read1_mid // self.res) + self.chrom_bin_offsets[read1_chrom][0]
+            read2_bin = (read2_mid // self.res) + self.chrom_bin_offsets[read2_chrom][0]
 
             bin_pair = tuple(sorted([read1_bin, read2_bin]))
+
+            # All:
+            if bin_pair not in self.tete:
+                self.all[bin_pair] = 0
+            self.all[bin_pair] += 1
 
             if 'TE' in line[4] and 'TE' in line[9]:
                 # TE <=> TE
                 if bin_pair not in self.tete:
                     self.tete[bin_pair] = 0
                 self.tete[bin_pair] += 1
+            elif 'TE' in line[4] or 'TE' in line[9]:
+                # TE <=> non-TE
+                if bin_pair not in self.tenn:
+                    self.tenn[bin_pair] = 0
+                self.tenn[bin_pair] += 1
+            else:
+                # non-TE <=> non-TE
+                if bin_pair not in self.nnnn:
+                    self.nnnn[bin_pair] = 0
+                self.nnnn[bin_pair] += 1
 
+            done += 1
             if done % 1000000 == 0:
-                print('Processed: {:,}'.format(stats_total_reads))
-                break
+                print('Processed: {:,}'.format(done))
+                #if done >20000000: break
 
         oh.close()
 
@@ -128,13 +145,33 @@ class build_matrices:
 
         # Save the matrices:
         # matrices are sparse:
-        # TE <=> TE
+        filename = os.path.join(out_path, self.sample_name, str(self.res), '%s_%s.all.raw.matrix' % (self.sample_name, self.res))
+        oh = open(filename, 'w')
+        for bins in sorted(self.all):
+            oh.write('%s\t%s\t%s\n' % (bins[0]+1, bins[1]+1, self.all[bins]))
+        oh.close()
+        print('Saved All matrix: "%s"' % filename)
+
         filename = os.path.join(out_path, self.sample_name, str(self.res), '%s_%s.tete.raw.matrix' % (self.sample_name, self.res))
         oh = open(filename, 'w')
         for bins in sorted(self.tete):
-            oh.write('%s\t%s\t%s\n' % (bins[0], bins[1], self.tete[bins]))
+            oh.write('%s\t%s\t%s\n' % (bins[0]+1, bins[1]+1, self.tete[bins]))
         oh.close()
-        print('Save TE <=> TE matrix: "%s"' % filename)
+        print('Saved TE <=> TE matrix: "%s"' % filename)
+
+        filename = os.path.join(out_path, self.sample_name, str(self.res), '%s_%s.tenn.raw.matrix' % (self.sample_name, self.res))
+        oh = open(filename, 'w')
+        for bins in sorted(self.tenn):
+            oh.write('%s\t%s\t%s\n' % (bins[0]+1, bins[1]+1, self.tenn[bins]))
+        oh.close()
+        print('Saved TE <=> non-TE matrix: "%s"' % filename)
+
+        filename = os.path.join(out_path, self.sample_name, str(self.res), '%s_%s.nnnn.raw.matrix' % (self.sample_name, self.res))
+        oh = open(filename, 'w')
+        for bins in sorted(self.nnnn):
+            oh.write('%s\t%s\t%s\n' % (bins[0]+1, bins[1]+1, self.nnnn[bins]))
+        oh.close()
+        print('Saved non-TE <=> non-TE matrix: "%s"' % filename)
 
 
 if __name__ == '__main__':
@@ -155,7 +192,7 @@ if __name__ == '__main__':
     script_path = os.path.dirname(os.path.realpath(__file__))
 
     mat = build_matrices(script_path, species, sys.argv[3], int(sys.argv[2]))
-    #mat.build_matrices(sys.argv[2], sys.argv[3])
+    mat.build_matrices(sys.argv[3])
     mat.save_matrices(sys.argv[4])
 
 
