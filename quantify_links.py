eea8d6b55f0d59812e03617f74892adc19c5bae1
oaxiom <oaxiom@localhost>
2019-03-10 22:00:49 +0800

    new enrichment measeruemnt system

--- quantify_links.py
+++ quantify_links.py
@@ -76,13 +76,13 @@ class quantify:
 
             if total % 1000000 == 0:
                 print('Processed: {:,}'.format(total))
-                #break
+                break
 
-            # MEasure TEs in detail
+            # Measure TEs in detail
             if 'TE' in r[4] and 'TE' in r[9]:
                 # possible to have more than one TE:
                 tel = [i.strip() for i in r[3].split(',') if ':' in i] # can also hoover up some genes, so use ':' to discriminate TEs
-                ter = [i.strip() for i in r[8].split(',') if ':' in i]
+                ter = [i.strip() for i in r[9].split(',') if ':' in i]
                 combs = product(tel, ter)
                 combs = [tuple(sorted(i)) for i in combs] # sort to make it unidirectional
 
@@ -96,7 +96,7 @@ class quantify:
                 if 'TE' in r[4]:
                     TE = [i.strip() for i in r[3].split(',') if ':' in i]
                 elif 'TE' in r[9]:
-                    TE = [i.strip() for i in r[8].split(',') if ':' in i]
+                    TE = [i.strip() for i in r[9].split(',') if ':' in i]
                 for t in TE:
                     if ':' not in t:
                         continue
@@ -119,38 +119,33 @@ class quantify:
         oh.close()
 
         oh_te_te = open('%s_te-te_anchor_frequencies.tsv' % self.project_name, 'w')
-        oh_te_te.write('%s\n' % '\t'.join(['TE1', 'TE2', 'count', '%']))
+        oh_te_te.write('%s\n' % '\t'.join(['TE1', 'TE2', 'count', 'RPM']))
         for k in sorted(list(res_te_te)):
-            oh_te_te.write('%s\t%s\t%s\t%s\n' % (k[0], k[1], res_te_te[k], res_te_te[k]/total*100.0))
+            te1 = self.genome._findDataByKeyLazy('name', k[0])
+            te2 = self.genome._findDataByKeyLazy('name', k[1])    
+            rpm = res_te_te[k]/total*1e6
+            joint_kb_size = te1['genome_count'] + te2['genome_count']
+            rpmpkbte = rpm / (joint_kb_size*e3)
+            line = {'te1': k[0], 'te2': k[1],
+                'rpm': rpm, 
+                'rpmpkbte': rpmpkbte,
+                'te1_genome_freq': te1['genome_percent'] / 100.0,
+                'te2_genome_freq': te2['genome_percent'] / 100.0,
+                #'enrichment': #!?!?! 
+                }
+            oh_te_te.write('{i[te1]}\t{i[te2]}\t{rpm}\t{rpmkbte}\t{te1_genome_freq}\t{te2_genome_freq}\n'.format(i=line))
         oh_te_te.close()
-
-        # How are you supposed to work this out?
-        #te_te = glbase3.genelist()
-        #te_te.load_list([{'name': str(k), 'count': res_te_nn[k]} for k in res_te_nn])
-        #te_te.map((genelist=self.genome, key='name')
-        #for te_pair in te_te:
-        #
-        #te_te._optimiseData()
-        #te_te.sort('name')
-        #te_te.saveTSV('%s_te-te_anchor_frequencies.tsv' % self.project_name)
 
         te_nn = glbase3.genelist()
         te_nn.load_list([{'name': k, 'count': res_te_nn[k]} for k in res_te_nn])
         te_nn = te_nn.map(genelist=self.genome, key='name')
         for te in te_nn:
-            te['percent'] = (res_te_nn[te['name']]/total) * 100.0
             te['RPM'] = (res_te_nn[te['name']]/total) * 1e6
-            te['RPM per kbp of TE'] = te['RPM'] / te['genome_count'] * 1e3
+            te['RPM per kbp of TE'] = te['RPM'] / (te['genome_count'] * 1e3)
             #te['enrichment'] =
         te_nn._optimiseData()
         te_nn.sort('name')
-        te_nn.saveTSV('%s_te-nn_anchor_frequencies.tsv' % self.project_name, key_order=['name', 'count', 'genome_count', 'percent', 'genome_percent', 'RPM'])
-
-        #oh_te_nn = open('%s_te-nn_anchor_frequencies.tsv' % self.project_name, 'w')
-        #oh_te_nn.write('%s\n' % '\t'.join(['TE1', 'count', '%']))
-        #for k in sorted(list(res_te_nn)):
-        #    oh_te_nn.write('%s\t%s\t%.6f\n' % (k, res_te_nn[k], res_te_nn[k]/total))
-        #oh_te_nn.close()
+        te_nn.saveTSV('%s_te-nn_anchor_frequencies.tsv' % self.project_name, key_order=['name', 'count', 'genome_count', 'genome_percent', 'RPM'])
 
         return
 
