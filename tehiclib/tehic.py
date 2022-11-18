209e3038126ef4afd0b1f2f1c241c0b17d2eff10
oaxiom <oaxiom@gmail.com>
2022-11-18 10:43:20 +0800

    added main class

---
+++ tehiclib/tehic.py
@@ -0,0 +1,41 @@
+
+
+
+
+class te_hic:
+    def __init__(self):
+        '''
+        Entry point for te_hic
+
+        '''
+        return
+
+    def stage1_collect_valid_pairs(self):
+        '''
+        **Stage 1**
+            Collect QC passing valid pairs
+        '''
+        return
+
+    def stage2_assign_to_genome_feature(self):
+        '''
+        **Stage 2**
+            Assign reads to a genome feature
+        '''
+        return
+
+    def stage3_quantify_links(self):
+        '''
+        **Stage 3**
+            Aggreate the genome contacts.
+
+        '''
+        return
+
+    def stage4_build_matrices(self):
+        '''
+        **Stage 4**
+            Build the matrices at the required resolutions
+
+        '''
+        return
