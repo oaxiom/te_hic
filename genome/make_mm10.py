e3705b140a70327050d0cc79fec0c32dccf4ba7d
oaxiom <oaxiom@gmail.com>
2022-12-16 10:04:28 +0800

    reenabled mm10

--- genome/make_mm10.py
+++ genome/make_mm10.py
@@ -10,15 +10,26 @@ This is for the mm10 genome
 
 import sys
 sys.path.append('../')
-from tehiclib import genelist
+from tehiclib import delayedlist, progressbar, genelist, common
+
+gtf = {
+    #"feature_type": 1,
+    "feature": 2,
+    "gtf_decorators": 8,
+    "commentlines": "#",
+    "loc": "location(chr=column[0], left=column[3], right=column[4])",
+    "strand": 6,
+    "skiplines": -1,
+    "force_tsv": True
+    }
 
 rmsk_track_form = {"force_tsv": True, 'loc': 'location(chr=column[5], left=column[6], right=column[7])',
     'repName': 10, 'repClass': 11, 'repFamily': 12}
 
-chr_set = frozenset(['X', 'Y'] + ['%s' % i for i in range(1, 30)])
+chr_set = frozenset(['X', 'Y'] + [str(i) for i in range(1, 23)])
 
 repeats = delayedlist(filename='mm10_rmsk.txt.gz', gzip=True, format=rmsk_track_form)
-gencode = delayedlist('gencode.vM20.annotation.gtf.gz', gzip=True, format=format.gtf)
+gencode = delayedlist('gencode.vM20.annotation.gtf.gz', gzip=True, format=gtf)
 
 keep_classes = frozenset(['LINE', 'LTR', 'SINE', 'DNA', 'Retroposon'])
 
@@ -32,40 +43,40 @@ p = progressbar(len(repeats))
 for idx, item in enumerate(repeats):
     if item['repClass'] not in keep_classes:
         continue
-
-    #print(item)
     if str(item['loc']['chr']) not in chr_set:
         continue
 
+    name = f"{item['repClass']}:{item['repFamily']}:{item['repName']}"
+
     newentry = {'loc': item['loc'],
-        'name': '%s:%s:%s' % (item['repName'], item['repFamily'], item['repClass']),
+        'name': name,
         'type': 'TE',
-        'ensg': '%s:%s:%s' % (item['repName'], item['repFamily'], item['repClass'])
+        'ensg': name
         }
     newl.append(newentry)
-    #print(newentry)
+
     added += 1
 
     #if idx > 100000:
     #    break
     p.update(idx)
 
-print('\nAdded %s features' % added)
+print(f'\nAdded {added:,} features')
+
+keep_gene_types = set(('protein_coding', 'lincRNA', 'lncRNA'))
 
 print('Gencode')
 p = progressbar(len(gencode))
 for idx, item in enumerate(gencode):
     if item['feature'] != 'exon':
         continue
-
-    #print(item)
-    if item['gene_type'] not in ('protein_coding', 'lincRNA'):
+    if item['gene_type'] not in keep_gene_types:
+        continue
+    if item['loc'].loc['chr'] not in chr_set:
         continue
 
-    if item['loc']['chr'] not in chr_set:
-        continue
-
-    newentry = {'loc': item['loc'],
+    newentry = {
+        'loc': item['loc'],
         'name': item['gene_name'],
         'type': item['gene_type'],
         'ensg': item['gene_id'].split('.')[0],
@@ -74,17 +85,19 @@ for idx, item in enumerate(gencode):
     added += 1
 
     if item['strand'] == '+':
-        prom_locs = {'loc': item['loc'].pointLeft(),
+        prom_locs = {
+            'loc': item['loc'].pointLeft(),
             'name': item['gene_name'],
             'type': item['gene_type'],
-            'ensg': item['gene_id'].split('.')[0],
+            'ensg': newentry['ensg'],
             'enst': item['transcript_id'].split('.')[0],
             }
     elif item['strand'] == '-':
-        prom_locs = {'loc': item['loc'].pointRight(),
+        prom_locs = {
+            'loc': item['loc'].pointRight(),
             'name': item['gene_name'],
             'type': item['gene_type'],
-            'ensg': item['gene_id'].split('.')[0],
+            'ensg': newentry['ensg'],
             'enst': item['transcript_id'].split('.')[0],
             }
     else:
@@ -97,12 +110,41 @@ for idx, item in enumerate(gencode):
 
     p.update(idx)
 
-print('\nAdded %s features' % added)
-
-gl = genelist()
-gl.load_list(newl)
-gl.save('mm10_glb_gencode_tes.glb')
+print(f'\nAdded {added:,} features')
 
 gl = genelist()
 gl.load_list(promoters)
 gl.save('mm10_glb_gencode_promoters.glb')
+
+gl = genelist()
+gl.load_list(newl)
+gl.save('mm10_glb_gencode_tes.glb')
+genome = gl
+
+tes = {}
+
+for idx, te in enumerate(genome):
+    if ':' not in te['name']:
+        continue # omit the genes
+    if '?' in te:
+        continue
+
+    if te['name'] not in tes:
+        tes[te['name']] = 0
+    tes[te['name']] += len(te['loc'])
+
+newl = []
+for k in tes:
+    newe = {'name': k,
+        'genome_count': tes[k],
+        'genome_percent': tes[k] / common.mm10_genome_size * 100.0}
+    newl.append(newe)
+
+gl = genelist()
+gl.load_list(newl)
+gl.sort('name')
+gl.saveTSV('mm10_te_genome_freqs.tsv', key_order=['name', 'genome_count', 'genome_percent'])
+gl.save('mm10_te_genome_freqs.glb')
+
+
+
