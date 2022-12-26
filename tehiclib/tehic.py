ff28e45fa108ae7e95b322677c6d851ebc7f0e1b
oaxiom <oaxiom@gmail.com>
2022-12-26 15:46:05 +0800

    save BEDPE intermediate files

--- tehiclib/tehic.py
+++ tehiclib/tehic.py
@@ -88,10 +88,10 @@ class te_hic:
         # TODO: convert the temp to a BEDPE;
         # save out the BEDPEs:
         self.logger.info('Saving BEDPE files')
-        all = gzip.open(f'stage2.all.{self.label}.bedpe.gz', 'w')
-        tete = gzip.open(f'stage2.tete.{self.label}.bedpe.gz', 'w')
-        tenn = gzip.open(f'stage2.tenn.{self.label}.bedpe.gz', 'w')
-        nnnn = gzip.open(f'stage2.nnnn.{self.label}.bedpe.gz', 'w')
+        file_all = gzip.open(f'stage2.all.{self.label}.bedpe.gz', 'wt')
+        file_tete = gzip.open(f'stage2.tete.{self.label}.bedpe.gz', 'wt')
+        file_tenn = gzip.open(f'stage2.tenn.{self.label}.bedpe.gz', 'wt')
+        file_nnnn = gzip.open(f'stage2.nnnn.{self.label}.bedpe.gz', 'wt')
         mapped_pairs_temp_file = open(self.mapped_pairs_temp_file, 'r')
 
         # File format:
@@ -100,23 +100,23 @@ class te_hic:
         for done, pair in enumerate(mapped_pairs_temp_file):
             pair = pair.strip().split('\t')
 
-            line = f'chr{pair[0]}\t{pair[1]}\t{pair[2]}\tchr{pair[3]}\t{pair[4]}\t{pair[5]}\t0\t{pair[10]}\t{pair[11]}\t{pair[6]}-{pair[7]}\t{pair[8]}-{pair[9]}\n'
+            line = f'chr{pair[0]}\t{pair[1]}\t{pair[2]}\tchr{pair[3]}\t{pair[4]}\t{pair[5]}\t0\t{pair[10]}\t{pair[11]}\t{pair[7]}-{pair[6]}\t{pair[9]}-{pair[8]}\n'
 
             # All:
-            all.write(line)
+            file_all.write(line)
 
             if 'TE' in pair[7] and 'TE' in pair[9]: # TE <=> TE
-                tete.write(line)
+                file_tete.write(line)
             elif 'TE' in pair[7] or 'TE' in pair[9]: # TE <=> non-TE
-                tenn.write(line)
+                file_tenn.write(line)
             else: # non-TE <=> non-TE
-                nnnn.write(line)
+                file_nnnn.write(line)
 
         mapped_pairs_temp_file.close()
-        all.close()
-        tete.close()
-        tenn.close()
-        nnnn.close()
+        file_all.close()
+        file_tete.close()
+        file_tenn.close()
+        file_nnnn.close()
 
         if not self.__save_intermediate_files:
             os.remove(self.valid_pairs_tmp_file) # not needed anymore
