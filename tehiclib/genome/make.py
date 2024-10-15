f3c7c3bf49674027976224f99dc18fd1b7aedb8b
oaxiom <oaxiom@gmail.com>
2024-10-15 15:22:02 +0800

    annotation path correct

--- tehiclib/genome/make.py
+++ tehiclib/genome/make.py
@@ -32,11 +32,11 @@ def make_index(genome, log):
     # Download data:
     chrom_sizes_path = f'{script_path}/../../genome/{genome}.chromSizes.gz'
     rmsk_path = f'{script_path}/../../genome/{genome}.rmsk.txt.gz'
-    #annotation_path = f'{script_path}/../../genome/{genome}.annotation.txt.gz'
+    annotation_path = f'{script_path}/../../genome/{genome}.annotation.txt.gz'
 
     subprocess.run(f'wget -c ftp://hgdownload.cse.ucsc.edu/goldenPath/{genome}/database/chromInfo.txt.gz -O {chrom_sizes_path}', shell=True)
     subprocess.run(f'wget -c http://hgdownload.soe.ucsc.edu/goldenPath/{genome}/database/rmsk.txt.gz -O {rmsk_path}', shell=True)
-    #subprocess.run(f"wget -c {genome_options[genome]['download']} -O {annotation_path}", shell=True)
+    subprocess.run(f"wget -c {genome_options[genome]['download']} -O {annotation_path}", shell=True)
 
     if genome_options[genome]['chrom_cleaner']:
         genome_options[genome]['chrom_cleaner'](genome)
